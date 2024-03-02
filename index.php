7a7ec4a7682fc5ec61446885e1d68b2ec02a9ca4
Gnosis <emmagab38@gmail.com>
2024-03-02 21:49:40 +0100

    commit changes

--- index.php
+++ index.php
@@ -46,6 +46,26 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+    <style>
+        .center {
+            margin: auto;
+            padding: 10px;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .profile_img{
+                margin-left: 65px;
+            }
+        }
+
+        .testimonials {
+            padding: 80px 0;
+            background: url(../img/house.webp) no-repeat;
+            background-position: center center;
+            background-size: cover;
+            position: relative;
+        }
+    </style>
 </head>
 
 <body>
@@ -135,6 +155,7 @@
                 You have a great deal under contract and you need funding, or you want to access capital with a cash-out refinance from property that you own.  I am a Certified Private Hard Money Broker with access to funds from hundreds of lender relationships.  Every deal is unique, and just like the right key opens a locked door, the details of your deal are key to finding you the right funding at the best rate and terms.
                 "Let's talk about getting your deal done!" - Mary Gervais, Certified Private Hard Money Broker
             </p>
+              <a href="#contact" style="align-items: center" class="col-6 offset-3 btn btn-primary rounded-pill ">Request a call back</a>
           </div>
         </div>
 
@@ -200,7 +221,8 @@
         </div>
 
       </div>
-    </section><!-- End Skills Section -->
+    </section>
+      <!-- End Skills Section -->
 
     <!-- ======= Services Section ======= -->
     <section id="services" class="services">
@@ -210,6 +232,21 @@
           <h3>INVESTING MADE SIMPLE </h3>
           <p style="font-weight: lighter">PRIVATE HARD MONEY FOR NON-OWNER OCCUPIED INVESTMENT PROPERTY</p>
         </div>
+          <div class="jumbotron jumbotron-fluid mb-5">
+              <div class="container">
+                  <img  src="img/1.jpg" class="col-lg-6 offset-lg-5 mb-3 profile_img" alt="20170328_163252x_edited.jpg" style="width: 200px; height: 200px; border-radius: 50%; "  >
+                  <h4 class="text-center">MARY GERVAIS - CERTIFIED PRIVATE HARD MONEY BROKER</h4>
+                  <div class="text-center">
+                      <p class="lead">For over 25 years I have worked as a real estate broker, commercial loan broker, escrow officer, newspaper reporter, photographer, and property investor as an entrepreneur.  I offer creative financing solutions for real estate investors and a full-service platform from application to closing that makes investing easy.</p>
+                      <i>"Do all the good you can, By all the means you can, In all the ways you can, In all the places you can, At all the times you can, To all the people you can, As long as ever you can."
+                          <br>
+                          John Wesley
+                      </i>
+
+                  </div>
+
+              </div>
+          </div>
 
         <div class="row">
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
@@ -246,6 +283,193 @@
     </section><!-- End Services Section -->
 
 
+      <section id="testimonials" class="testimonials">
+          <div class="container aos-init aos-animate" data-aos="zoom-in">
+
+              <div style="visibility: hidden" class="testimonials-slider swiper swiper-initialized swiper-horizontal swiper-pointer-events aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                  <div class="swiper-wrapper" id="swiper-wrapper-de10a9d2105390cd37" aria-live="off" style="transition-duration: 600ms; transform: translate3d(-7776px, 0px, 0px);"><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="0" role="group" aria-label="1 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                              <h3>Saul Goodman</h3>
+                              <h4>Ceo &amp; Founder</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="1" role="group" aria-label="2 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                              <h3>Sara Wilsson</h3>
+                              <h4>Designer</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="2" role="group" aria-label="3 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
+                              <h3>Jena Karlis</h3>
+                              <h4>Store Owner</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" role="group" aria-label="4 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
+                              <h3>Matt Brandon</h3>
+                              <h4>Freelancer</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="4" role="group" aria-label="5 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
+                              <h3>John Larson</h3>
+                              <h4>Entrepreneur</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div>
+
+                      <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="0" role="group" aria-label="1 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                              <h3>Saul Goodman</h3>
+                              <h4>Ceo &amp; Founder</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><!-- End testimonial item -->
+
+                      <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="1" role="group" aria-label="2 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                              <h3>Sara Wilsson</h3>
+                              <h4>Designer</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><!-- End testimonial item -->
+
+                      <div class="swiper-slide swiper-slide-next" data-swiper-slide-index="2" role="group" aria-label="3 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
+                              <h3>Jena Karlis</h3>
+                              <h4>Store Owner</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><!-- End testimonial item -->
+
+                      <div class="swiper-slide" data-swiper-slide-index="3" role="group" aria-label="4 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
+                              <h3>Matt Brandon</h3>
+                              <h4>Freelancer</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><!-- End testimonial item -->
+
+                      <div class="swiper-slide" data-swiper-slide-index="4" role="group" aria-label="5 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
+                              <h3>John Larson</h3>
+                              <h4>Entrepreneur</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><!-- End testimonial item -->
+
+                      <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="0" role="group" aria-label="1 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                              <h3>Saul Goodman</h3>
+                              <h4>Ceo &amp; Founder</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="1" role="group" aria-label="2 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                              <h3>Sara Wilsson</h3>
+                              <h4>Designer</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="2" role="group" aria-label="3 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
+                              <h3>Jena Karlis</h3>
+                              <h4>Store Owner</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" role="group" aria-label="4 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
+                              <h3>Matt Brandon</h3>
+                              <h4>Freelancer</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div><div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="4" role="group" aria-label="5 / 5">
+                          <div class="testimonial-item">
+                              <img src="https://bootstrapmade.com/demo/templates/BizLand/assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
+                              <h3>John Larson</h3>
+                              <h4>Entrepreneur</h4>
+                              <p>
+                                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                              </p>
+                          </div>
+                      </div></div>
+                  <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 2" aria-current="true"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 3"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 4"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 5"></span></div>
+                  <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+
+          </div>
+      </section>
 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
